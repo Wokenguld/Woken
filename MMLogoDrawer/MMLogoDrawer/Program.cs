ab805ed5fb03460c1fea447d23e2f476bf0d857b
Wokenguld <59863173+Wokenguld@users.noreply.github.com>
2020-02-11 09:15:27 +0200

    MM

---
+++ MMLogoDrawer/MMLogoDrawer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MMLogoDrawer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
